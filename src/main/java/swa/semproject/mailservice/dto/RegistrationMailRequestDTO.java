2bc100195cba3d17e8a4a65fc73681eabf2acdb3
matthewGithib <matejposta8@gmail.com>
2024-07-08 14:32:44 +0200

    serialization fix

--- src/main/java/swa/semproject/mailservice/dto/RegistrationMailRequestDTO.java
+++ src/main/java/swa/semproject/mailservice/dto/RegistrationMailRequestDTO.java
@@ -3,9 +3,11 @@ package swa.semproject.mailservice.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class RegistrationMailRequestDTO {
 
