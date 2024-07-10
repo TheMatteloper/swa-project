1d5d81ea21483bf8c2d0c9cd250c1cd6f0d265fa
novakl39 <novakl39@fel.cvut.cz>
2024-07-10 19:43:22 +0200

    Add JsonSerialize

--- integration-tests/src/main/java/cvut/swa/integrationtest/dto/ReservationResponseDTO.java
+++ integration-tests/src/main/java/cvut/swa/integrationtest/dto/ReservationResponseDTO.java
@@ -2,6 +2,8 @@ package cvut.swa.integrationtest.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import cvut.swa.integrationtest.enums.ReservationStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,9 +21,11 @@ public class ReservationResponseDTO {
 
     private Integer roomId;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime timeFrom;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime timeTo;
 
